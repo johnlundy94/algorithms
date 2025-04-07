28a4339777a2d9d8009c09e2d80ec913d006f094
John Lundy <johnclundyy@gmail.com>
2025-04-07 11:46:20 -0600

    Comments

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -9,19 +9,29 @@
 const l1 = [2, 4, 3];
 const l2 = [5, 6, 4];
 
+// We are defining a function that takes in two linked lists. Each node has a .val and a .next
 var addTwoNumbers = function (l1, l2) {
+  // We use a dummy head node so that we don't have to write separate logic for the first node in the result list. Think of this like a placeholder to simplify appending new nodes.
   let dummyHead = new ListNode(0);
+  // This keeps track of the current position in the new linked list we're building. It starts at the dummy node.
   let current = dummyHead;
+  // This is to store the carry over value when the sum of the two digits is >_ 10
   let carry = 0;
 
+  // This loop continues as long as there is still something left in l1, or something left in l2 or there's a leftover carry
   while (l1 !== null || l2 !== null || carry > 0) {
+    // We're pulling out the .val from each node. If a list is already done (null), we treat its value as 0. Remember that these are ternary operators (condition ? value_if_true : value_if_false)
     let val1 = l1 !== null ? l1.val : 0;
     let val2 = l2 !== null ? l2.val : 0;
 
+    // We're adding digit from l1 to digit from l2 plus any leftover carry from last time
     let sum = val1 + val2 + carry;
+    // If sum is 14, we carry 1 for the next round.
     carry = Math.floor(sum / 10);
+    // The digit to store in the new list is just sum % 10. E.g., if sum =14, we store 4.
     current.next = new ListNode(sum % 10);
 
+    // Advance the current pointer so the next digit can be added at the end
     current = current.next;
     if (l1 !== null) l1 = l1.next;
     if (l2 !== null) l2 = l2.next;
