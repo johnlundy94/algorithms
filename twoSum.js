8437e8bb22d6042ff74cb4bd482eb7096c0f2643
John Lundy <johnclundyy@gmail.com>
2025-04-21 11:20:13 -0600

    Addded study comments to twoSum

--- twoSum.js
+++ twoSum.js
@@ -15,24 +15,35 @@
 // Output: [1,2]
 // Example 3:
 
+// Defining input
 const nums = [3, 3];
 const target = 6;
 // Output: [0,1]
 
-//We want to create a map instance and then set each nums[i] to the map. We want to then check if the nums[i] - target is equal to a number that we already have in our array. Return that number
-
+// Declares a function twoSum that takes two parameters, the nums array and the target sum, the function will then return an array of two indices.
 var twoSum = function (nums, target) {
+  // Creates a Map objects to store numbers from nums as keys and their indices as values. A Map object allows for O(1) because you are able to pass through it one time. Keys will be the numbers and the values will be their indices.
   const numsMap = new Map();
 
+  // Iterates through the array nums. Each iteration processes one number and checks if its complement exists in numsMap.
   for (let i = 0; i < nums.length; i++) {
+    // Calculates the complement, or the number needed to reach target when added to nums[i]. Keep in mind that complement is a query we use to check if a number we need exists in numsMap.
     const complement = target - nums[i];
 
+    // This checks if the complement exists as a key in numsMap.
     if (numsMap.has(complement)) {
+      // This returns the indeces of the two numbers that sum to target. numsMap.get(complement) retrieves the index j where complement was stored, or in other words in is retrieving the index of the number (key) that was previously stored in numsMap using set(). i is the current index. This sets up the return to be [j, i]
       return [numsMap.get(complement), i];
     }
+    // Stores the current number and its index in numsMap for future complement checks.
     numsMap.set(nums[i], i);
   }
+  // returns an array if no solution was found.
   return [];
 };
 
 console.log(twoSum(nums, target));
+
+// .has(key) checks if a key exists.
+// .get(key) Retrieves the value associated with a key.
+// .set(key, value) Adds or updates a key-value pair in the Map.
